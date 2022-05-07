ebce9223f445cc17c036bcbcdd98ebf2d262e32d
Md Hasibur Rahman <100030004+Hasibur445@users.noreply.github.com>
2022-05-07 09:08:25 +0600

    Add files via upload

--- login system with avatar/mysql databse table.sql
+++ login system with avatar/mysql databse table.sql
@@ -19,5 +19,20 @@ CREATE TABLE `user_form` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL,
+  `usertype` varchar(100) NOT NULL default 'user',
+
+
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+
+
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
+
