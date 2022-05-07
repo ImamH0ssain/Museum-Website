5123b61fa9435a331fbffe43692e71388f6e2024
Md Hasibur Rahman <100030004+Hasibur445@users.noreply.github.com>
2022-05-07 21:02:35 +0600

    Add files via upload

---
+++ presentation/login system with avatar/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">Shelby's Collection</a>
+
+      <nav class="navbar">
+         <a href="login.php">login</a>
+         <a href="products.php">view products</a>
+      </nav>
+
+   </div>
+
+</header>
